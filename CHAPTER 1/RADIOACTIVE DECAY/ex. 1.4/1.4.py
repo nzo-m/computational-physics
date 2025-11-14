b44056738cfb7460d2a0f6067d79ca354a1a2145
Lawrence <co.renso.m@gmail.com>
2025-11-14 08:31:15 +0800

    1.4 fix

--- CHAPTER 1/RADIOACTIVE DECAY/ex. 1.4/1.4.py
+++ CHAPTER 1/RADIOACTIVE DECAY/ex. 1.4/1.4.py
@@ -1,31 +1,41 @@
-from nzo import plot
+import numpy as np
+import matplotlib.pyplot as plt
+import scienceplots
 
-NA = 10000
-NB = 10000
-TB = 100
-ta_tb = float(input("TA/TB Ratio: "))
-TA = TB * ta_tb
+NA0=1000
+NB0=0
+dt=0.01
+tf=5
+ts=np.arange(0,tf+dt,dt)
 
-t = 1
-dt = 0.01
-tmax = 1000
-T = []
-na = []
-nb = []
+ta=[1,0.5,1]
+tb=[0.5,1,1]
 
-while t <= tmax:
-    T.append(t)
-    na.append(NA)
-    nb.append(NB)
+plt.style.use(['science','grid','bright'])
+fig, axes = plt.subplots(1, 3, figsize=(15,4))
+fig.suptitle("Radioactive Decay with Varying Ratios")
 
-    dNA = -NA / TA * dt
-    dNB = (NA / TA - NB / TB) * dt
-    NA += dNA
-    NB += dNB
-    t += dt
+for i in range(3):
+    NA = np.zeros_like(ts, dtype=float)
+    NB = np.zeros_like(ts, dtype=float)
 
-plot(T, na, T, nb,
-     title="Radioactive Decay with TA/TB = %.4f" % ta_tb,
-     xlabel="Time (s)",
-     ylabel="Number of atoms",
-     labels=["NA(t)", "NB(t)"])
+    NA[0] = NA0
+    NB[0] = NB0
+
+    for j in range(len(ts)-1):
+        dNA = -NA[j]/ta[i]
+        dNB = NA[j]/ta[i] - NB[j]/tb[i]
+        NA[j+1] = NA[j] + dNA*dt
+        NB[j+1] = NB[j] + dNB*dt
+
+    ax = axes[i]
+    ax.plot(ts, NA, label="NA")
+    ax.plot(ts, NB, label="NB")
+
+    ax.set_title(f"TA/TB = {ta[i]}/{tb[i]}")
+    ax.set_xlabel("Time (s)")
+    ax.set_ylabel("Atoms")
+    ax.legend()
+
+plt.savefig("radioactive-decay.png", dpi=300, bbox_inches='tight')
+plt.show()
