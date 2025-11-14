143975cfcd987702a2635931eb0b33eee9f746e4
Lawrence <co.renso.m@gmail.com>
2025-11-14 08:13:28 +0800

    1.1fix

--- CHAPTER 1/RADIOACTIVE DECAY/ex. 1.1/1.1.py
+++ CHAPTER 1/RADIOACTIVE DECAY/ex. 1.1/1.1.py
@@ -1,26 +1,36 @@
-from nzo import plot
+import numpy as np
+import matplotlib.pyplot as plt
+import scienceplots
 
 v0 = 0
 t0 = 0
 g = 9.8
 dt = 0.1
-tmax = 10
+tf = 10
 
-vs = []
+vs_euler= []
 ts = []
 v = v0
 t = t0
 
-while t <= tmax:
+while t <= tf:
+    vs_euler.append(v)
     ts.append(t)
-    vs.append(v)
-    v -= g * dt
+    v = v - g * dt
     t += dt
 
-plot(ts, vs,
-    title="Euler Method: Free Fall",
-    xlabel="Time (s)",
-    ylabel="Velocity (m/s)",
-    labels=["v(t)"])
 
+ts_exact = np.linspace(0, tf, 200)
+vs_exact = -g*ts_exact
+
+plt.style.use(['science', 'grid', 'bright'])
+plt.figure(figsize=(6,4))
+plt.plot(ts, vs_euler, '-', label='Euler Approx.', lw=2)
+plt.plot(ts_exact, vs_exact, '--', label='Exact Solution', lw=2)
+plt.xlabel('Time(s)')
+plt.ylabel('Velocity (m/s)')
+plt.title('Free Fall (Euler vs. Exact)')
+plt.savefig("free-fall.png", dpi=300, bbox_inches='tight')
+plt.legend()
+plt.show()
 
