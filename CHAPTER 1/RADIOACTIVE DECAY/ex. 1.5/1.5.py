2c2528d91d3a62e237298eb00afc8ecc3be06177
Lawrence <co.renso.m@gmail.com>
2025-11-14 08:39:01 +0800

    1.5 fix

--- CHAPTER 1/RADIOACTIVE DECAY/ex. 1.5/1.5.py
+++ CHAPTER 1/RADIOACTIVE DECAY/ex. 1.5/1.5.py
@@ -1,41 +1,42 @@
-from nzo import plot
+import numpy as np
+import matplotlib.pyplot as plt
+import scienceplots
 
 tau = 1.0
 dt = 0.001
 t_max = 10
+ts = np.arange(0, t_max + dt, dt)
 
 initial = [
     (100, 0),
     (0, 100),
     (50, 50)]
 
-for NA0, NB0 in initial:
-    NA = NA0
-    NB = NB0
-    time = 0
+plt.style.use(['science', 'grid', 'bright'])
+fig, axes = plt.subplots(1, 3, figsize=(15, 4))
+fig.suptitle("A-B System for Different Initial Conditions")
 
-    ts = []
-    NAs = []
-    NBs = []
+for i, (NA0, NB0) in enumerate(initial):
+    NA = np.zeros_like(ts)
+    NB = np.zeros_like(ts)
 
-    # Euler method
-    while time <= t_max:
-        ts.append(time)
-        NAs.append(NA)
-        NBs.append(NB)
+    NA[0] = NA0
+    NB[0] = NB0
 
-        dNA = (NB - NA) / tau
-        dNB = (NA - NB) / tau
+    for j in range(len(ts) - 1):
+        dNA = (NB[j] - NA[j]) / tau
+        dNB = (NA[j] - NB[j]) / tau
 
-        NA += dNA * dt
-        NB += dNB * dt
+        NA[j+1] = NA[j] + dNA * dt
+        NB[j+1] = NB[j] + dNB * dt
 
-        time += dt
+    ax = axes[i]
+    ax.plot(ts, NA, label="NA(t)")
+    ax.plot(ts, NB, label="NB(t)")
+    ax.set_title(f"NA0 = {NA0}, NB0 = {NB0}")
+    ax.set_xlabel("Time (s)")
+    ax.set_ylabel("Number of Nuclei")
+    ax.legend(loc="best")
 
-    plot(
-        ts, NAs, ts, NBs,
-        title=f"A ↔ B, tau={tau}s, NA0={NA0}, NB0={NB0}",
-        xlabel="Time [s]",
-        ylabel="Number of Nuclei",
-        labels=["NA(t)", "NB(t)"]
-    )
+fig.savefig("varying-initial-conditions.png", dpi=300, bbox_inches='tight')
+plt.show()
