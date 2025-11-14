0d09313648b120c95b4df6ca19da0486bca7e569
Lawrence <co.renso.m@gmail.com>
2025-11-14 08:25:58 +0800

    1.2 compare

--- CHAPTER 1/RADIOACTIVE DECAY/ex. 1.2/1.2.py
+++ CHAPTER 1/RADIOACTIVE DECAY/ex. 1.2/1.2.py
@@ -1,21 +1,32 @@
-from nzo import plot
+import numpy as np
+import matplotlib.pyplot as plt
+import scienceplots
 
 x0 = 0
 t0 = 0
-dt = 0.01
-tmax = 10
-ts = []
+v = 40
+dt = 0.1
+tf = 10
+
 xs = []
+ts = []
 
-while t0 <= tmax:
-    v = 40.0
-    xs.append(x0)
-    ts.append(t0)
-    x0 += v * dt
+while t0 <= tf:
+    x0 = x0 + v*dt
     t0 += dt
+    ts.append(t0)
+    xs.append(x0)
 
-plot(ts, xs,
-    title="Euler Method: Horizontal Motion",
-    xlabel="Time (s)",
-    ylabel="Horizontal Distance x (m)",
-    labels=["x(t)"])
+texact = np.linspace(0, tf, 200)
+xexact = v*texact
+
+plt.style.use(['science', 'grid', 'bright'])
+plt.figure(figsize=(6,4))
+plt.plot(ts, xs, '-', label='Euler Approx.', lw=2)
+plt.plot(texact, xexact, '--', label='Exact Solution', lw=2)
+plt.xlabel("Time(s)")
+plt.ylabel("Horizontal Motion x (m)")
+plt.title("Horizontal Motion (Euler vs. Exact)")
+plt.legend()
+plt.savefig("horizontal-motion.png", dpi=300, bbox_inches='tight')
+plt.show()
